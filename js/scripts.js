02f19307eff925ea9ba5c575c4f30d3cb336677d
Prathamesh Pandya <111297857+Om-Devloper@users.noreply.github.com>
2023-10-07 20:47:43 +0530

    all small changes

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,30 @@ function whatsapp() {
     shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`;
     window.open(shareUrl, "_blank");
 }
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    occupation: document.getElementById("occupation").value,
+    type: document.getElementById("type").value,
+    contact: document.getElementById("contact").value,
+  };
+
+  const serviceID = "service_jzdfcs2";
+  const templateID = "template_wv2kx8k";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("contact").value = "";
+        document.getElementById("type").value = "";
+        document.getElementById("occupation").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+        window.location.replace("https://rohitpradhan.com/payment.html");
+
+    })
+    .catch(err=>console.log(err));
+
+}
